6f3495cb6a8a7147aa16b43d0c5e494e7e5b71c2
Chamikara Jayarathne <cjayarathne@virtusa.com>
2015-12-04 17:17:42 -0500

    Fixed mysql scripts

--- src/main/resources/db/mysql/initDB.sql
+++ src/main/resources/db/mysql/initDB.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS vets (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
+  profile_description VARCHAR(255),
   INDEX(last_name)
 ) engine=InnoDB;
 
@@ -37,6 +38,7 @@ CREATE TABLE IF NOT EXISTS owners (
   address VARCHAR(255),
   city VARCHAR(80),
   telephone VARCHAR(20),
+  profile_description VARCHAR(255),
   INDEX(last_name)
 ) engine=InnoDB;
 
@@ -46,6 +48,7 @@ CREATE TABLE IF NOT EXISTS pets (
   birth_date DATE,
   type_id INT(4) UNSIGNED NOT NULL,
   owner_id INT(4) UNSIGNED NOT NULL,
+  profile_description VARCHAR(255),
   INDEX(name),
   FOREIGN KEY (owner_id) REFERENCES owners(id),
   FOREIGN KEY (type_id) REFERENCES types(id)
